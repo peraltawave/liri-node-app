2b80a0030a276845e36559c9aa5814f679358975
Miguel Peralta <peraltawave@gmail.com>
2018-11-02 01:58:10 -0700

    final push

---
+++ keys.js
@@ -0,0 +1,31 @@
+console.log('this is loaded for miggie smalls yo');
+
+// exports.twitter = {
+//     consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// };
+
+
+exports.twitter = {
+    consumer_key: 'W4FgaEUwSkrpWTnTHaz2UDWIQ',
+    consumer_secret: 'C2qWEnolIuvFd3GlHnvqWH1uTSTitKTEw6iUHrbMkZDuwQyNHf',
+    access_token_key: '366417916-YX8ImUOTKPUWyvKZB6QHVVH2cPZNWqE52fuiOZGl',
+    access_token_secret: 'kvUBhYtVRPflRw2ZR5OAnXCgfZgsEYzP8uMoDfw93K5Gs',
+  }
+
+
+
+
+
+// exports.spotify = { // mig
+//     id: '3ec65794ed744c748b11a6d8202f2b2b',
+//     secret: 'd3790f564a814d3198c3658b78b13b05'
+// };
+
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
